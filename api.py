35fd26b2e638a7d39541fa38f88aa3f82ca468d8
isha dalvi <dipeshxyzyadav@gmail.com>
2025-10-21 14:45:03 +0530

    Update api.py

--- api.py
+++ api.py
@@ -91,7 +91,7 @@ def generate_key():
 def shorten_url(long_url):
     try:
         random_suffix = secrets.token_hex(3)[:6]
-        alias = f"REDW{random_suffix}"
+        alias = f"movie4uyt{random_suffix}"
         shortener_url = (
             f"https://nowshort.com/api?api={SHORTENER_API_KEY}"
             f"&url={requests.utils.quote(long_url)}"
