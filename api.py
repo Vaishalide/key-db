f1aaa71176182d948b471a52369e0490ec19eb5d
isha dalvi <dipeshxyzyadav@gmail.com>
2025-10-23 12:21:50 +0530

    Update api.py

--- api.py
+++ api.py
@@ -91,7 +91,7 @@ def generate_key():
 def shorten_url(long_url):
     try:
         random_suffix = secrets.token_hex(3)[:6]
-        alias = f"movie4uyt{random_suffix}"
+        alias = f"pwthorx{random_suffix}"
         shortener_url = (
             f"https://arolinks.com/api?api={SHORTENER_API_KEY}"
             f"&url={requests.utils.quote(long_url)}"
