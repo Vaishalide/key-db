32341724a04af45a7e8927f5132d562e3217c1dd
isha dalvi <dipeshxyzyadav@gmail.com>
2025-10-24 13:34:39 +0530

    Update api.py

--- api.py
+++ api.py
@@ -27,7 +27,7 @@ GITHUB_REPO_NAME = "Key-DB"
 GITHUB_ACCESS_TOKEN = "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"
 TOKEN_EXPIRY_MINUTES = 30  # <-- UPDATED to 30 minutes as requested
 KEY_EXPIRY_DAYS = 1
-SHORTENER_API_KEY = "d9918049795aad4d2d193e317ac522f1d276c701"
+SHORTENER_API_KEY = "14cfebd76f5bb80680ebff03144bc7af69acac51"
 
 IST = pytz.timezone('Asia/Kolkata')
 
@@ -93,7 +93,7 @@ def shorten_url(long_url):
         random_suffix = secrets.token_hex(3)[:6]
         alias = f"pwthorx{random_suffix}"
         shortener_url = (
-            f"https://arolinks.com/api?api={SHORTENER_API_KEY}"
+            f"https://nowshort.com/api?api={SHORTENER_API_KEY}"
             f"&url={requests.utils.quote(long_url)}"
             f"&alias={alias}"
         )
